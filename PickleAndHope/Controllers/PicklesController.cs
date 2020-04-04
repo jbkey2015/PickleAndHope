a6bd042111d9eb8e038b6878d46c35ffc041c9e4
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-04-04 13:25:31 -0500

    Add project files.

---
+++ PickleAndHope/Controllers/PicklesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PickleAndHope.DataAccess;
+using PickleAndHope.Models;
+
+namespace PickleAndHope.Controllers
+{
+    [Route("api/pickles")]
+    [ApiController]
+    public class PicklesController : ControllerBase
+    {
+        PickleRepository _repository = new PickleRepository();
+
+        // api/pickles
+        [HttpPost]
+        public IActionResult AddPickle(Pickle pickleToAdd)
+        {
+            var existingPickle = _repository.GetByType(pickleToAdd.Type);
+            if (existingPickle == null)
+            {
+                _repository.Add(pickleToAdd);
+                return Created("", pickleToAdd);
+            }
+            else
+            {
+                var updatedPickle = _repository.Update(pickleToAdd);
+                return Ok(updatedPickle);
+            }
+        }
+
+        // api/pickles
+        [HttpGet]
+        public IActionResult GetAllPickles()
+        {
+            var allPickles = _repository.GetAll();
+
+            return Ok(allPickles);
+        }
+
+        // api/pickles/{id}
+        // api/pickles/5
+        [HttpGet("{id}")]
+        public IActionResult GetPickleById(int id)
+        {
+            var pickle = _repository.GetById(id);
+
+            if (pickle == null) return NotFound("No pickle with that id could be found.");
+
+            return Ok(pickle);
+        }
+
+    }
+}
