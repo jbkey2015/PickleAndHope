cce9c3745fcea543eaff51779369596ad112e1c4
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-06-13 11:49:22 -0500

    configuration and Dependency Injectionf

---
+++ pickleandhope.ui/src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Pickles from  '../components/pages/Pickles/Pickles'
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-danger">Bootstrap Button</button>
+      <Pickles/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pickleandhope.ui/src/components/pages/Pickles/Pickles.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import SinglePickle from "../../shared/SinglePickle";
+import {getPickles} from "../../../helpers/data/pickleData";
+
+class Pickles extends Component {
+
+  state = {pickles: [
+    //{id:1, type:"dill", numberInStock:12, size: "large", price:5}
+  ]};
+
+  componentDidMount(){
+    getPickles().then(pickles => this.setState({pickles:pickles}));
+  }
+
+  render() {
+
+    const {pickles} = this.state;
+    return pickles.map(pickle => <SinglePickle key={pickle.id} pickle={pickle}/>);
+  }
+
+}
+
+export default Pickles;
